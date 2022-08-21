276934f235de301d2220b91868441216dfa70750
Dimitrije Cvetković <dimitrije.le.cvetkovic@gmail.com>
2022-08-21 05:32:37 +0200

    _

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // AOS.init();
+
+var projects = document.getElementById("projects").getBoundingClientRect();
+var about = document.getElementById("about").getBoundingClientRect();
+var contact = document.getElementById("contact").getBoundingClientRect();
+
+
+const li = document.querySelectorAll(".linking");
+
+const sec = document.querySelectorAll(".sections");
+// console.log(sec)
+
+function active() {
+  let len = sec.length;
+  while (--len && window.scrollY + 300 < sec[len].offsetTop) {}
+  li.forEach((ltx) => ltx.classList.remove("active"));
+  li[len].classList.add("active");
+}
+
+active();
+window.addEventListener("scroll", active);
