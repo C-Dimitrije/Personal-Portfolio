4b5ff110068892a177626a091ace90025af09f59
Dimitrije Cvetković <dimitrije.le.cvetkovic@gmail.com>
2022-08-26 05:09:00 +0200

    _

--- script/script.js
+++ script/script.js
@@ -8,14 +8,28 @@ var contact = document.getElementById("contact").getBoundingClientRect();
 const li = document.querySelectorAll(".linking");
 
 const sec = document.querySelectorAll(".sections");
-// console.log(sec)
 
 function active() {
   let len = sec.length;
-  while (--len && window.scrollY + 300 < sec[len].offsetTop) {}
+  while (--len && window.scrollY + 400 < sec[len].offsetTop) {}
   li.forEach((ltx) => ltx.classList.remove("active"));
   li[len].classList.add("active");
 }
 
 active();
 window.addEventListener("scroll", active);
+
+
+AOS.init();
+        var loader = document.getElementById("loader");
+        loader.style.display = "block";
+        
+        
+        window.addEventListener("load", delayl() )
+        
+        function delayl(){
+            setTimeout(function(){
+                loader.style.display = "none";
+            }, 650)
+            // window.addEventListener("load", function () {})
+        }
